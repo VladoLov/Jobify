487299b2e699c2a377a1f7c5c455b8424a1108ec
VladoLov <vladislav.lovric@gmail.com>
2024-09-24 21:28:13 +0200

    update inut form

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import * as z from "zod";
+/* import * as z from "zod"; */
 
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
